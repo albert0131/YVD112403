bb8e8d0975540560d3331c70ca2e8f4766ee52b8
albert <alber@gmail.com>
2016-11-24 11:23:35 +0800

    add publishProgress and onPostExecute 顯示進度

--- app/src/main/java/com/example/user/yvd112403/MainActivity.java
+++ app/src/main/java/com/example/user/yvd112403/MainActivity.java
@@ -4,17 +4,19 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView);
         MyTask task = new MyTask();
         task.execute(5);
     }
-    // --- 使用 AsyncTask 執行多行緒, 顯示進度
+    // --- 使用抽象類別 AsyncTask 執行多行緒, 顯示進度
     class MyTask extends AsyncTask<Integer, Integer, Integer> {
 
         @Override
@@ -23,13 +25,26 @@ public class MainActivity extends AppCompatActivity {
             for (int i=0;i<=n;i++)
             {
                 Log.d("TASK", String.valueOf(i));
+                publishProgress(i);
                 try {
                     Thread.sleep(500);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
-            return null;
+            return 100;
+        }
+        // --- 顯示進度與結果
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            tv.setText(String.valueOf(values[0]));
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+            super.onPostExecute(integer);
+            tv.setText(String.valueOf(integer) + '%' + "  Complete!");
         }
     }
 }
